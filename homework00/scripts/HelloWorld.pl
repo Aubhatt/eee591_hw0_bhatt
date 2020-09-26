bd0965195279d5f87ef7c23fc8091b992ac28089
arthbhatt <arth3697@gmail.com>
2020-09-26 01:27:39 -0700

    Q3: HelloWorld.pl

--- homework00/scripts/HelloWorld.pl
+++ homework00/scripts/HelloWorld.pl
@@ -1,1 +1,6 @@
 # Your HelloWorld program here 
+use strict; # Use a strict interpretation
+use warnings FATAL=>qw(all); # Turn warnings into errors
+use diagnostics; # Print helpful info, for errors
+
+print STDOUT "WARNING ... Perl is fun\n";
